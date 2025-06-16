e9d2a8977a29456661c1ac7fd43de7d47fab619f
aravindav <aravindwaitz@gmail.com>
2025-06-16 17:04:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from Node.js app!');
+// });
+
+// const PORT = process.env.PORT || 80;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require('express');
 const app = express();
 
@@ -5,8 +17,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js app!');
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
